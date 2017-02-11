ba9d1c7e576fcbcb0c00aff51f9c63e285a93fb3
luowen <bigpao.luo@gmail.com>
2017-02-11 11:07:08 +0800

    add support jsp

---
+++ web/sjl-web/src/main/java/com/lianying/shangjialian/web/controller/HomeController.java
@@ -0,0 +1,46 @@
+package com.lianying.shangjialian.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by luowen on 2017/2/10.
+ */
+@Controller
+@RequestMapping(value = "/home")
+public class HomeController {
+
+    @RequestMapping
+    @ResponseBody
+    public String home() {
+        return "home";
+    }
+
+    @RequestMapping(value = "/test/{name}")
+    public String homeView(@PathVariable String name, Model model) {
+        model.addAttribute("a", "-a-");
+        model.addAttribute("b", name);
+        System.out.println("name = " + name);
+        return "home";
+    }
+
+    @RequestMapping("/info")
+    @ResponseBody
+    public Map<String, String> info() {
+        Map<String, String> hashMap = new HashMap<String, String>();
+
+
+
+        hashMap.put("name", "luowen");
+        hashMap.put("age", "28");
+        hashMap.put("lover", "maomao");
+        return hashMap;
+    }
+
+}
